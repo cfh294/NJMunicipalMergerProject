9137ebc42bc09faee8bbe4c0ceda0f8acece0d83
Connor Hornibrook <cfbrooks94@gmail.com>
2016-01-04 12:33:52 -0500

    fixed muncode error in ocean county

---
+++ Data Cleanup/code_correction.sql
@@ -0,0 +1,15 @@
+UPDATE borders
+SET sourcecode='1508'
+WHERE sourcemun='TOMS RIVER TWP';
+
+UPDATE borders
+SET adjcode='1508'
+WHERE adjmun='TOMS RIVER TWP';
+
+UPDATE borders
+SET sourcecode='1509'
+WHERE sourcemun='EAGLESWOOD TWP';
+
+UPDATE borders
+SET adjcode='1509'
+WHERE adjmun='EAGLESWOOD TWP';
